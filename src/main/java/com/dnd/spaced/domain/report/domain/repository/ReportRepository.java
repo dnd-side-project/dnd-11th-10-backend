ca4f06581024960e2f63f2fb0d38136b7c6b7e13
apptie <appti@naver.com>
2024-08-05 18:51:38 +0900

    feature :: add report repository

---
+++ src/main/java/com/dnd/spaced/domain/report/domain/repository/ReportRepository.java
@@ -0,0 +1,8 @@
+package com.dnd.spaced.domain.report.domain.repository;
+
+import com.dnd.spaced.domain.report.domain.Report;
+
+public interface ReportRepository {
+
+    void save(Report report);
+}
