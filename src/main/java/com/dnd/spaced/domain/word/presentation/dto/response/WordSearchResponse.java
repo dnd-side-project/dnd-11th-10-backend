d5de23ae8b0141f792fb598ae6e30c79e7ab8cf9
최미래 <miraechoi@dgsw.hs.kr>
2024-08-07 21:11:13 +0900

    refactor :: refactor to enforce dto layer boundaries

--- src/main/java/com/dnd/spaced/domain/word/presentation/dto/response/WordSearchResponse.java
+++ src/main/java/com/dnd/spaced/domain/word/presentation/dto/response/WordSearchResponse.java
@@ -1,11 +1,9 @@
 package com.dnd.spaced.domain.word.presentation.dto.response;
 
-import com.dnd.spaced.domain.word.domain.repository.dto.response.WordSearchDto;
-
 import java.util.List;
 
 public record WordSearchResponse(
-        List<WordSearchDto> words,
+        List<WordSearchInfoResponse> words,
         String lastWordName
 ) {
 }
