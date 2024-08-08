987ec11e5036a8cd8ef814c93096c60671b23911
apptie <appti@naver.com>
2024-08-08 19:05:46 +0900

    feature :: add bookmarking word find api

--- src/main/java/com/dnd/spaced/domain/bookmark/presentation/BookmarkController.java
+++ src/main/java/com/dnd/spaced/domain/bookmark/presentation/BookmarkController.java
@@ -1,11 +1,20 @@
 package com.dnd.spaced.domain.bookmark.presentation;
 
 import com.dnd.spaced.domain.bookmark.application.BookmarkService;
+import com.dnd.spaced.domain.bookmark.application.dto.BookmarkServiceMapper;
+import com.dnd.spaced.domain.bookmark.application.dto.request.BookmarkConditionInfoDto;
+import com.dnd.spaced.domain.bookmark.application.dto.response.BookmarkWordInfoDto;
+import com.dnd.spaced.domain.bookmark.presentation.dto.BookmarkControllerMapper;
+import com.dnd.spaced.domain.bookmark.presentation.dto.request.BookmarkConditionRequest;
+import com.dnd.spaced.domain.bookmark.presentation.dto.response.BookmarkWordResponse;
 import com.dnd.spaced.global.controller.ResponseEntityConst;
 import com.dnd.spaced.global.resolver.auth.AuthAccount;
 import com.dnd.spaced.global.resolver.auth.AuthAccountInfo;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +26,25 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @PostMapping("/words/{id}/bookmark")
-    public ResponseEntity<Void> processBookmark(
-            @AuthAccount AuthAccountInfo accountInfo,
-            @PathVariable Long id
-    ) {
+    public ResponseEntity<Void> processBookmark(@AuthAccount AuthAccountInfo accountInfo, @PathVariable Long id) {
         bookmarkService.processBookmark(accountInfo.email(), id);
 
         return ResponseEntityConst.NO_CONTENT;
+    }
+
+    @GetMapping("/bookmarks")
+    public ResponseEntity<BookmarkWordResponse> findAllBy(
+            @AuthAccount AuthAccountInfo accountInfo,
+            BookmarkConditionRequest request,
+            Pageable pageable
+    ) {
+        BookmarkConditionInfoDto dto = BookmarkServiceMapper.of(
+                accountInfo.email(),
+                request.lastBookmarkId(),
+                pageable
+        );
+        List<BookmarkWordInfoDto> result = bookmarkService.findAllBy(dto);
+
+        return ResponseEntity.ok(BookmarkControllerMapper.from(result));
     }
 }
