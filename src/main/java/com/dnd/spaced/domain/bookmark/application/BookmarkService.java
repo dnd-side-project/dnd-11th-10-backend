7c622cef7c4de9fc1acc31dec3578db866e21501
apptie <appti@naver.com>
2024-08-08 19:03:06 +0900

    feature :: add bookmarking word find service function

--- src/main/java/com/dnd/spaced/domain/bookmark/application/BookmarkService.java
+++ src/main/java/com/dnd/spaced/domain/bookmark/application/BookmarkService.java
@@ -2,16 +2,25 @@ package com.dnd.spaced.domain.bookmark.application;
 
 import com.dnd.spaced.domain.account.domain.Account;
 import com.dnd.spaced.domain.account.domain.repository.AccountRepository;
+import com.dnd.spaced.domain.bookmark.application.dto.BookmarkServiceMapper;
+import com.dnd.spaced.domain.bookmark.application.dto.request.BookmarkConditionInfoDto;
+import com.dnd.spaced.domain.bookmark.application.dto.response.BookmarkWordInfoDto;
 import com.dnd.spaced.domain.bookmark.domain.Bookmark;
 import com.dnd.spaced.domain.bookmark.domain.repository.BookmarkRepository;
+import com.dnd.spaced.domain.bookmark.domain.repository.dto.BookmarkRepositoryMapper;
+import com.dnd.spaced.domain.bookmark.domain.repository.dto.request.BookmarkConditionDto;
+import com.dnd.spaced.domain.bookmark.domain.repository.dto.response.BookmarkWordDto;
 import com.dnd.spaced.domain.word.application.exception.ForbiddenBookmarkException;
 import com.dnd.spaced.domain.word.application.exception.WordNotFoundException;
 import com.dnd.spaced.domain.word.domain.Word;
 import com.dnd.spaced.domain.word.domain.repository.WordRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BookmarkService {
 
@@ -20,7 +29,7 @@ public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
     public void processBookmark(String email, Long wordId) {
-        Account account = accountRepository.findBy(email).orElseThrow(ForbiddenBookmarkException::new);
+        Account account = findAccount(email);
         Word word = wordRepository.findBy(wordId).orElseThrow(WordNotFoundException::new);
 
         bookmarkRepository.findBy(account.getId(), word.getId()).ifPresentOrElse(bookmarkRepository::delete, () -> {
@@ -28,5 +37,22 @@ public class BookmarkService {
 
             bookmarkRepository.save(bookmark);
         });
+    }
+
+    public List<BookmarkWordInfoDto> findAllBy(BookmarkConditionInfoDto dto) {
+        Account account = findAccount(dto.email());
+        BookmarkConditionDto bookmarkConditionDto = BookmarkRepositoryMapper.of(
+                account.getId(),
+                dto.lastBookmarkId(),
+                dto.pageable()
+        );
+        List<BookmarkWordDto> result = bookmarkRepository.findAllBy(bookmarkConditionDto);
+
+        return BookmarkServiceMapper.from(result);
+    }
+
+    private Account findAccount(String email) {
+        return accountRepository.findBy(email)
+                                .orElseThrow(ForbiddenBookmarkException::new);
     }
 }
