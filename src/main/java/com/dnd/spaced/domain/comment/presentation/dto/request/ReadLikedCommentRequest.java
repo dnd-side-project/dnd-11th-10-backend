afb7a522b85d9bcf0a4cc1764250b007a7b3da94
apptie <appti@naver.com>
2024-08-14 12:27:17 +0900

    docs :: update comment api documentation

--- src/main/java/com/dnd/spaced/domain/comment/presentation/dto/request/ReadLikedCommentRequest.java
+++ src/main/java/com/dnd/spaced/domain/comment/presentation/dto/request/ReadLikedCommentRequest.java
@@ -1,4 +1,10 @@
 package com.dnd.spaced.domain.comment.presentation.dto.request;
 
-public record ReadLikedCommentRequest(Long lastCommentId) {
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+
+public record ReadLikedCommentRequest(
+        @Schema(description = "마지막으로 조회한 댓글 ID", requiredMode = RequiredMode.NOT_REQUIRED)
+        Long lastCommentId
+) {
 }
--- src/main/java/com/dnd/spaced/domain/comment/presentation/dto/response/LikedCommentResponse.java
+++ src/main/java/com/dnd/spaced/domain/comment/presentation/dto/response/LikedCommentResponse.java
@@ -1,11 +1,18 @@
 package com.dnd.spaced.domain.comment.presentation.dto.response;
 
 import com.dnd.spaced.domain.comment.application.dto.response.LikedCommentDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
-public record LikedCommentResponse(List<CommentResponse> comments, Long lastCommentId) {
+public record LikedCommentResponse(
+        @Schema(description = "댓글 정보")
+        List<CommentResponse> comments,
+
+        @Schema(description = "마지막으로 조회한 댓글 ID", nullable = true)
+        Long lastCommentId
+) {
 
     public static LikedCommentResponse of(List<LikedCommentDto> dtos, String baseUrl, String imageUri) {
         if (dtos.isEmpty()) {
@@ -20,12 +27,25 @@ public record LikedCommentResponse(List<CommentResponse> comments, Long lastComm
     }
 
     public record CommentResponse(
+            @Schema(description = "댓글 ID")
             Long commentId,
+
+            @Schema(description = "용어 ID")
             Long wordId,
+
+            @Schema(description = "댓글 내용")
             String content,
+
+            @Schema(description = "댓글 ID")
             int likeCount,
+
+            @Schema(description = "댓글 생성 일자")
             LocalDateTime createdAt,
+
+            @Schema(description = "댓글 수정 일자")
             LocalDateTime updatedAt,
+
+            @Schema(description = "댓글 작성자 정보")
             WriterInfoResponse writerInfo
     ) {
 
@@ -49,8 +69,13 @@ public record LikedCommentResponse(List<CommentResponse> comments, Long lastComm
     }
 
     public record WriterInfoResponse(
+            @Schema(description = "회원 ID")
             Long id,
+
+            @Schema(description = "회원 닉네임 ID")
             String nickname,
+
+            @Schema(description = "회원 프로필 이미지")
             String profileImage
     ) {
     }
