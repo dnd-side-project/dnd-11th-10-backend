35cd0a6a12e71ad72a7ce980ea4c59df4b13e48c
최미래 <miraechoi@dgsw.hs.kr>
2024-08-16 21:25:25 +0900

    refactor :: remove unused imports

--- src/main/java/com/dnd/spaced/domain/quiz/domain/QuizResult.java
+++ src/main/java/com/dnd/spaced/domain/quiz/domain/QuizResult.java
@@ -1,9 +1,7 @@
 package com.dnd.spaced.domain.quiz.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
