a50d26394b95714c908229d60f3feb584365b3ef
최미래 <miraechoi@dgsw.hs.kr>
2024-08-19 08:39:43 +0900

    refactor :: remove unused import

--- src/main/java/com/dnd/spaced/domain/quiz/presentation/QuizController.java
+++ src/main/java/com/dnd/spaced/domain/quiz/presentation/QuizController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
