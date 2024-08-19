ed6f329201e66a196d97be998ab75bc94fbe13fc
최미래 <miraechoi@dgsw.hs.kr>
2024-08-19 18:21:11 +0900

    remove :: remove unused findResultsByQuizId and findByName

--- src/main/java/com/dnd/spaced/domain/quiz/domain/repository/QuerydslQuizRepository.java
+++ src/main/java/com/dnd/spaced/domain/quiz/domain/repository/QuerydslQuizRepository.java
@@ -2,6 +2,8 @@ package com.dnd.spaced.domain.quiz.domain.repository;
 
 import com.dnd.spaced.domain.quiz.domain.*;
 import com.dnd.spaced.domain.word.domain.Category;
+import com.dnd.spaced.domain.word.domain.exception.InvalidCategoryException;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -11,35 +13,32 @@ import java.util.Optional;
 
 import static com.dnd.spaced.domain.quiz.domain.QQuizQuestion.quizQuestion;
 import static com.dnd.spaced.domain.quiz.domain.QQuizResult.quizResult;
+import static com.dnd.spaced.domain.word.domain.QWord.word;
 
 @Repository
 @RequiredArgsConstructor
 public class QuerydslQuizRepository implements QuizRepository {
 
     private final JPAQueryFactory queryFactory;
+    private static final String IGNORE_CATEGORY = "전체";
 
     @Override
-    public List<QuizQuestion> findQuestionsByCategory(Category category) {
+    public List<QuizQuestion> findQuestionsByCategory(String categoryName) {
         return queryFactory.selectFrom(quizQuestion)
-                .where(quizQuestion.category.eq(category))
+                .where(categoryEq(categoryName))
                 .fetch();
     }
 
-    @Override
-    public Optional<QuizResult> findResultsByQuizId(Long quizId) {
-        QuizResult result = queryFactory.selectFrom(quizResult)
-                .where(quizResult.quizQuestion.quiz.id.eq(quizId))
-                .fetchOne();
+    private BooleanExpression categoryEq(String category) {
+        if (category == null || IGNORE_CATEGORY.equals(category)) {
+            return null;
+        }
 
-        return Optional.ofNullable(result);
-    }
-
-    @Override
-    public Optional<QuizQuestion> findByName(String categoryName) {
-        QuizQuestion result = queryFactory.selectFrom(quizQuestion)
-                .where(quizQuestion.category.eq(Category.valueOf(categoryName)))
-                .fetchOne();
-
-        return Optional.ofNullable(result);
+        try {
+            Category categoryEnum = Category.findBy(category);
+            return quizQuestion.category.eq(categoryEnum);
+        } catch (InvalidCategoryException e) {
+            return null;
+        }
     }
 }
--- src/main/java/com/dnd/spaced/domain/quiz/domain/repository/QuizRepository.java
+++ src/main/java/com/dnd/spaced/domain/quiz/domain/repository/QuizRepository.java
@@ -1,14 +1,9 @@
 package com.dnd.spaced.domain.quiz.domain.repository;
 
 import com.dnd.spaced.domain.quiz.domain.QuizQuestion;
-import com.dnd.spaced.domain.quiz.domain.QuizResult;
-import com.dnd.spaced.domain.word.domain.Category;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface QuizRepository {
-    List<QuizQuestion> findQuestionsByCategory(Category category);
-    Optional<QuizResult> findResultsByQuizId(Long quizId);
-    Optional<QuizQuestion> findByName(String categoryName);
+    List<QuizQuestion> findQuestionsByCategory(String categoryName);
 }
