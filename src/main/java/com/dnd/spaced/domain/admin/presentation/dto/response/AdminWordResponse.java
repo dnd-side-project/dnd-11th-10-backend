53c6cb6f527eaba63b181e118cf9723133071a66
최미래 <miraechoi@dgsw.hs.kr>
2024-08-21 08:40:36 +0900

    docs :: add admin presentation response documentation

--- src/main/java/com/dnd/spaced/domain/admin/presentation/dto/response/AdminWordResponse.java
+++ src/main/java/com/dnd/spaced/domain/admin/presentation/dto/response/AdminWordResponse.java
@@ -1,12 +1,25 @@
 package com.dnd.spaced.domain.admin.presentation.dto.response;
 
 import com.dnd.spaced.domain.word.domain.Pronunciation;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record AdminWordResponse(
+
+        @Schema(description = "용어 ID")
         Long id,
+
+        @Schema(description = "용어")
         String name,
+
+        @Schema(description = "발음")
         Pronunciation pronunciation,
+
+        @Schema(description = "뜻")
         String meaning,
+
+        @Schema(description = "카테고리")
         String category,
+
+        @Schema(description = "예문")
         String example
 ) {}
