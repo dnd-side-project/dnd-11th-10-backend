ed1b9ae8b35da86435d56b51c599ddae12140510
최미래 <miraechoi@dgsw.hs.kr>
2024-08-20 23:53:45 +0900

    feature :: add quiz api documentation

--- src/main/java/com/dnd/spaced/domain/quiz/presentation/dto/request/QuizRequest.java
+++ src/main/java/com/dnd/spaced/domain/quiz/presentation/dto/request/QuizRequest.java
@@ -1,4 +1,5 @@
 package com.dnd.spaced.domain.quiz.presentation.dto.request;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
@@ -7,9 +8,11 @@ import java.util.List;
 public record QuizRequest(
 
         @NotNull
+        @Schema(description = "용어 카테고리", allowableValues = {"개발", "디자인", "비즈니스"}, nullable = false)
         String categoryName,
 
         @NotEmpty
+        @Schema(description = "정답 ID", nullable = false)
         List<Long> answerIds
 ) {
 }
--- src/main/java/com/dnd/spaced/domain/quiz/presentation/dto/response/QuizResponse.java
+++ src/main/java/com/dnd/spaced/domain/quiz/presentation/dto/response/QuizResponse.java
@@ -1,11 +1,16 @@
 package com.dnd.spaced.domain.quiz.presentation.dto.response;
 
 import com.dnd.spaced.domain.quiz.domain.QuizQuestion;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record QuizResponse(
+
+        @Schema(description = "퀴즈 ID")
         Long quizId,
+
+        @Schema(description = "용어 퀴즈 목록")
         List<QuizQuestion> questions
 ) {
 }
