ccbb0b8eb71777828f4992345a2afde15bb32051
최미래 <miraechoi@dgsw.hs.kr>
2024-08-20 19:47:31 +0900

    feature :: add finds to report repository

--- src/main/java/com/dnd/spaced/domain/report/domain/repository/ReportRepository.java
+++ src/main/java/com/dnd/spaced/domain/report/domain/repository/ReportRepository.java
@@ -2,7 +2,16 @@ package com.dnd.spaced.domain.report.domain.repository;
 
 import com.dnd.spaced.domain.report.domain.Report;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReportRepository {
 
     void save(Report report);
+
+    Optional<Report> findById(Long id);
+
+    void deleteById(Long id);
+
+    List<Report> findReportsAfterId(Long lastReportId, int size);
 }
