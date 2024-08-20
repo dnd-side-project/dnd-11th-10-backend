66a33d15298fd505765238470387babb5aa7b7ff
최미래 <miraechoi@dgsw.hs.kr>
2024-08-20 19:45:57 +0900

    feature :: add admin report comment entity

---
+++ src/main/java/com/dnd/spaced/domain/admin/domain/AdminReportComment.java
@@ -0,0 +1,30 @@
+package com.dnd.spaced.domain.admin.domain;
+
+import com.dnd.spaced.domain.comment.domain.Comment;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(callSuper = false, of = "id")
+public class AdminReportComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reported_comment_id")
+    private Comment reportedComment;
+
+}
