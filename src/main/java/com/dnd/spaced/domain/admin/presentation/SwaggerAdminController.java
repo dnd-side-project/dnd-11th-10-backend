9a461d3f972e2c363c548552c1299c7cbd6d7e7f
최미래 <miraechoi@dgsw.hs.kr>
2024-08-21 18:01:27 +0900

    docs :: fix admin api documentation

--- src/main/java/com/dnd/spaced/domain/admin/presentation/SwaggerAdminController.java
+++ src/main/java/com/dnd/spaced/domain/admin/presentation/SwaggerAdminController.java
@@ -4,6 +4,8 @@ import com.dnd.spaced.domain.admin.application.dto.request.AdminWordRequestDto;
 import com.dnd.spaced.domain.admin.presentation.dto.response.AdminWordResponse;
 import com.dnd.spaced.domain.admin.presentation.dto.response.ReportListResponse;
 import com.dnd.spaced.global.docs.annotation.ExceptionSpec;
+import com.dnd.spaced.global.docs.annotation.ExcludeCommonHeaderSpec;
+import com.dnd.spaced.global.docs.annotation.NotRequiredCommonHeaderSpec;
 import com.dnd.spaced.global.exception.ExceptionCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -21,21 +23,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Tag(name = "관리자 관련 API", description = "용어 생성, 용어 삭제, 용어 수정, 신고 수락, 신고 무시, 신고 목록 조회")
 public interface SwaggerAdminController {
 
+    @ExcludeCommonHeaderSpec
     @Operation(summary = "용어 등록", description = "관리자가 새로운 용어를 등록합니다.")
     @ApiResponse(responseCode = "201", description = "Created")
-    @ExceptionSpec(values = {
-            ExceptionCode.VALIDATION_ERROR,
-            ExceptionCode.UNAUTHORIZED
-    })
+    @ExceptionSpec(values = {ExceptionCode.UNAUTHORIZED})
     ResponseEntity<Void> createWord(
             @Parameter(hidden = true) Authentication authentication,
             @RequestBody @Valid AdminWordRequestDto wordRequestDto
     );
 
+    @ExcludeCommonHeaderSpec
     @Operation(summary = "용어 삭제", description = "관리자가 용어를 삭제합니다.")
     @ApiResponse(responseCode = "204", description = "No Content")
     @ExceptionSpec(values = {
-            ExceptionCode.INVALID_PATH_VARIABLE,
             ExceptionCode.WORD_NOT_FOUND,
             ExceptionCode.UNAUTHORIZED
     })
@@ -44,12 +44,11 @@ public interface SwaggerAdminController {
             @Parameter(description = "용어 ID") @PathVariable Long id
     );
 
+    @ExcludeCommonHeaderSpec
     @Operation(summary = "용어 수정", description = "관리자가 용어를 수정합니다.")
     @ApiResponse(responseCode = "204", description = "No Content")
     @ExceptionSpec(values = {
-            ExceptionCode.INVALID_PATH_VARIABLE,
             ExceptionCode.WORD_NOT_FOUND,
-            ExceptionCode.VALIDATION_ERROR,
             ExceptionCode.UNAUTHORIZED
     })
     ResponseEntity<Void> updateWord(
@@ -58,10 +57,10 @@ public interface SwaggerAdminController {
             @RequestBody @Valid AdminWordRequestDto wordRequestDto
     );
 
+    @ExcludeCommonHeaderSpec
     @Operation(summary = "용어 조회", description = "관리자가 용어를 조회합니다.")
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = AdminWordResponse.class)))
     @ExceptionSpec(values = {
-            ExceptionCode.INVALID_PATH_VARIABLE,
             ExceptionCode.WORD_NOT_FOUND,
             ExceptionCode.UNAUTHORIZED
     })
@@ -69,10 +68,10 @@ public interface SwaggerAdminController {
             @Parameter(description = "용어 ID") @PathVariable Long id
     );
 
+    @ExcludeCommonHeaderSpec
     @Operation(summary = "신고 수락", description = "관리자가 신고를 수락합니다.")
     @ApiResponse(responseCode = "204", description = "No Content")
     @ExceptionSpec(values = {
-            ExceptionCode.INVALID_PATH_VARIABLE,
             ExceptionCode.REPORT_NOT_FOUND,
             ExceptionCode.REPORTED_COMMENT_NOT_FOUND,
             ExceptionCode.COMMENT_NOT_FOUND,
@@ -82,22 +81,20 @@ public interface SwaggerAdminController {
             @Parameter(description = "신고 ID") @PathVariable Long id
     );
 
+    @ExcludeCommonHeaderSpec
     @Operation(summary = "신고 무시", description = "관리자가 신고를 무시합니다.")
     @ApiResponse(responseCode = "204", description = "No Content")
     @ExceptionSpec(values = {
-            ExceptionCode.INVALID_PATH_VARIABLE,
-            ExceptionCode.REPORT_NOT_FOUND,
-            ExceptionCode.UNAUTHORIZED
+            ExceptionCode.REPORT_NOT_FOUND
     })
     ResponseEntity<Void> ignoreReport(
             @Parameter(description = "신고 ID") @PathVariable Long id
     );
 
+    @NotRequiredCommonHeaderSpec
     @Operation(summary = "신고 목록 조회", description = "관리자가 신고 목록을 조회합니다.")
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ReportListResponse.class)))
-    @ExceptionSpec(values = {
-            ExceptionCode.UNAUTHORIZED
-    })
+    @ExceptionSpec(values = {ExceptionCode.UNAUTHORIZED})
     ResponseEntity<ReportListResponse> findReports(
             @Parameter(description = "마지막으로 조회한 신고 ID", required = false) @RequestParam(required = false) Long lastReportId
     );
