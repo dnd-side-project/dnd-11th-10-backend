ace285bdd3108a1851f5acfb6b86a5671aee63b9
최미래 <miraechoi@dgsw.hs.kr>
2024-09-03 11:17:11 +0900

    hotfix :: remove @ConfigurationPropertiesScan

--- src/main/java/com/dnd/spaced/SpacedApplication.java
+++ src/main/java/com/dnd/spaced/SpacedApplication.java
@@ -2,10 +2,8 @@ package com.dnd.spaced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class SpacedApplication {
 
 	public static void main(String[] args) {
