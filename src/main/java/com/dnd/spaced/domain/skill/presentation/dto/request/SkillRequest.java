4c5535ce85b1e07187a4438d5f32306b41874a2e
최미래 <miraechoi@dgsw.hs.kr>
2024-09-11 17:26:39 +0900

    feature :: add getter and setter

--- src/main/java/com/dnd/spaced/domain/skill/presentation/dto/request/SkillRequest.java
+++ src/main/java/com/dnd/spaced/domain/skill/presentation/dto/request/SkillRequest.java
@@ -1,7 +1,11 @@
 package com.dnd.spaced.domain.skill.presentation.dto.request;
 
 import com.dnd.spaced.domain.skill.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public record SkillRequest(
         Long correctCount,
         Category category
