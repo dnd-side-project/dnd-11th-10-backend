5f9b2ced347bb2a347b7178710be20059b0e0fcc
최미래 <miraechoi@dgsw.hs.kr>
2024-09-11 17:33:00 +0900

    remove :: remove getter and setter

--- src/main/java/com/dnd/spaced/domain/skill/presentation/dto/request/SkillRequest.java
+++ src/main/java/com/dnd/spaced/domain/skill/presentation/dto/request/SkillRequest.java
@@ -1,11 +1,7 @@
 package com.dnd.spaced.domain.skill.presentation.dto.request;
 
 import com.dnd.spaced.domain.skill.domain.Category;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public record SkillRequest(
         Long correctCount,
         Category category
