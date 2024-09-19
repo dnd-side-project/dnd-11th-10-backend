cabc1b74f2632d5523762797c9a438f62545b634
최미래 <miraechoi@dgsw.hs.kr>
2024-09-19 15:43:30 +0900

    feature :: add from method to multiple popular comment info dto

--- src/main/java/com/dnd/spaced/domain/comment/application/dto/response/MultiplePopularCommentInfoDto.java
+++ src/main/java/com/dnd/spaced/domain/comment/application/dto/response/MultiplePopularCommentInfoDto.java
@@ -1,6 +1,7 @@
 package com.dnd.spaced.domain.comment.application.dto.response;
 
 import com.dnd.spaced.domain.comment.domain.repository.dto.response.PopularCommentInfoDto;
+import com.dnd.spaced.domain.comment.domain.repository.dto.response.PopularCommentWithoutIsLikeDto;
 import java.time.LocalDateTime;
 
 public record MultiplePopularCommentInfoDto(
@@ -26,6 +27,21 @@ public record MultiplePopularCommentInfoDto(
                 dto.likeAccountId() != null
         );
     }
+
+    public static MultiplePopularCommentInfoDto from(PopularCommentWithoutIsLikeDto dto) {
+        PronunciationInfoDto pronunciationInfo = new PronunciationInfoDto(dto.pronunciation().getEnglish());
+
+        return new MultiplePopularCommentInfoDto(
+                dto.commentId(),
+                new WordInfoDto(dto.wordId(), dto.wordName(), dto.wordCategory().getName(), pronunciationInfo),
+                dto.content(),
+                dto.likeCount(),
+                dto.createdAt(),
+                dto.updatedAt(),
+                false
+        );
+    }
+
 
     public record WordInfoDto(Long id, String name, String categoryName, PronunciationInfoDto pronunciationInfo) {
     }
