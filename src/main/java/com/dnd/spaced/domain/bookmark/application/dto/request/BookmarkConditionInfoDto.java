937aa4646c7288eaaabb637609708bcbd8f71e55
최미래 <miraechoi@dgsw.hs.kr>
2024-09-30 17:59:42 +0900

    feature :: add category to bookmark dto

--- src/main/java/com/dnd/spaced/domain/bookmark/application/dto/request/BookmarkConditionInfoDto.java
+++ src/main/java/com/dnd/spaced/domain/bookmark/application/dto/request/BookmarkConditionInfoDto.java
@@ -2,5 +2,5 @@ package com.dnd.spaced.domain.bookmark.application.dto.request;
 
 import org.springframework.data.domain.Pageable;
 
-public record BookmarkConditionInfoDto(String email, Long lastBookmarkId, Pageable pageable) {
+public record BookmarkConditionInfoDto(String email, String category, Long lastBookmarkId, Pageable pageable) {
 }
