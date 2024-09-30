af95969fb059a60acb0bdbf8cb2adbea048ca14f
최미래 <miraechoi@dgsw.hs.kr>
2024-09-30 18:02:56 +0900

    feature :: add category to bookmark request and response

--- src/main/java/com/dnd/spaced/domain/bookmark/presentation/dto/request/BookmarkConditionRequest.java
+++ src/main/java/com/dnd/spaced/domain/bookmark/presentation/dto/request/BookmarkConditionRequest.java
@@ -1,4 +1,15 @@
 package com.dnd.spaced.domain.bookmark.presentation.dto.request;
 
-public record BookmarkConditionRequest(Long lastBookmarkId) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record BookmarkConditionRequest(
+        @Schema(
+                description = "용어 카테고리",
+                allowableValues = {"전체 실무", "디자인", "개발", "비즈니스"},
+                requiredMode = Schema.RequiredMode.NOT_REQUIRED
+        )
+        String category,
+
+        @Schema(description = "마지막으로 조회한 북마크 Id", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+        Long lastBookmarkId) {
 }
