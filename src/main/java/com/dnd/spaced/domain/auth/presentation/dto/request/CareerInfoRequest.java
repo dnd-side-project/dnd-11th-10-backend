7bfd48a174c1c11090d0a568eecb682e097918a5
최미래 <miraechoi@dgsw.hs.kr>
2024-10-27 19:54:27 +0900

    hotfix :: add career info request allowableValue

--- src/main/java/com/dnd/spaced/domain/auth/presentation/dto/request/CareerInfoRequest.java
+++ src/main/java/com/dnd/spaced/domain/auth/presentation/dto/request/CareerInfoRequest.java
@@ -10,7 +10,7 @@ public record CareerInfoRequest(
         @Schema(description = "회사", allowableValues = {"비공개", "대기업", "중견기업", "중소기업", "스타트업", "외국계", "취준생/인턴"})
         @NotBlank String company,
 
-        @Schema(description = "연차", allowableValues = {"1년 차 미만", "1~2년 차", "2~3년 차", "3~4년 차", "4~5년 차", "5년 차 이상", "취준생/인턴", "비공개"})
+        @Schema(description = "연차", allowableValues = {"비공개", "1년 차 미만", "1~2년 차", "2~3년 차", "3~4년 차", "4~5년 차", "5년 차 이상"})
         @NotBlank String experience
 ) {
 }
